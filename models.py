3b6f6edda93c624daba13b156597a6fe5ce71516
Vladyslav Susiak <vladyslavsusiak@gmail.com>
2026-05-05 13:10:50 +0300

    Phase 2 commit

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import enum
-from sqlalchemy import Column, Integer, Float, String, Date, Time, Enum
+from sqlalchemy import Column, Integer, Float, String, Date, Time, Enum, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 
@@ -22,18 +23,28 @@ class WindDirection(enum.Enum):
     NNW = "NNW"
 
 
+class WindData(Base):
+    __tablename__ = "wind_data"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    weather_id = Column(Integer, ForeignKey("weather_data.id"), unique=True)
+
+    wind_degree = Column(Integer, nullable=False)
+    wind_kph = Column(Float, nullable=False)
+    wind_direction = Column(Enum(WindDirection), nullable=False)
+    wind_mph = Column(Float, nullable=True)
+    gust_kph = Column(Float, nullable=True)
+    gust_mph = Column(Float, nullable=True)
+
+    weather = relationship("Weather", back_populates="wind_info")
+
 class Weather(Base):
     __tablename__ = "weather_data"
 
     id = Column(Integer, primary_key=True, index=True)
-
     country = Column(String, nullable=False)
-    wind_degree = Column(Integer, nullable=False)
-    wind_kph = Column(Float, nullable=False)
-    wind_direction = Column(Enum(WindDirection), nullable=False)
     last_updated = Column(Date, nullable=False)
     sunrise = Column(Time, nullable=True)
 
-    wind_mph = Column(Float, nullable=True)
-    gust_kph = Column(Float, nullable=True)
-    gust_mph = Column(Float, nullable=True)
+    wind_info = relationship("WindData", back_populates="weather", uselist=False)
